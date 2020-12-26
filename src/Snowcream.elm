de046d976526a17483b55bf24f78330773165804
jesseilev <jesseilev@gmail.com>
2020-12-26 14:21:13 -0500

    nicer styling

--- src/Snowcream.elm
+++ src/Snowcream.elm
@@ -1,8 +1,13 @@
 module Snowcream exposing (..)
 
 
+-- TODO 
+-- nice slider ui
+-- download image
+
 import Triangle as T exposing (Triangle)
 import Browser
+import Browser.Dom as Dom
 import Debug
 import Html exposing (Html)
 import Html.Attributes
@@ -31,7 +36,8 @@ import Json.Decode as Decode
 -- MODEL 
 type alias Model = 
   { tModels : List (TModel TData) 
-  , targetCount : Int 
+  , targetCount : Int
+  , bgColor : Color
   }
 
 type alias TData = 
@@ -41,20 +47,24 @@ type alias Flags = ()
 
 init : Flags -> (Model, Cmd Msg)
 init flags = 
-  { tModels = [ ]
+  ({ tModels = [ ]
   , targetCount = 0
+  , bgColor = Color.red
   }
-    |> update (ChangeTargetCount 1)
+  , Random.generate GenBackground colorGenerator 
+  )
 
-triRoot : Random.Generator (TModel TData)
-triRoot = 
+    -- |> Dom.getViewport
+
+triRoot : Color -> Random.Generator (TModel TData)
+triRoot bgColor = 
   Random.map4
     (\r g b a ->
       { location = Point2d.origin |> Point2d.translateBy (Vector2d.pixels 0 -unit)
       , scalevel = 2
       , qt = T.QT0
       , mirrors = [ Axis2d.through Point2d.origin Direction2d.y ]
-      , data = { color = Color.rgba r g b 1 }
+      , data = { color = Color.weightedMix bgColor (Color.rgba r g b a) 0.1 }
       }
     )
    (Random.float 0 1) (Random.float 0 1) (Random.float 0 1) (Random.float 0.4 0.6)
@@ -64,7 +74,13 @@ growOrPruneTree newTc model =
   let newTModels = List.take newTc model.tModels in
   ( { model | targetCount = newTc, tModels = newTModels }
   , if List.length newTModels < newTc then 
-      Random.generate ChildT (childGenerator newTModels)
+      Cmd.batch 
+        [ Random.generate ChildT (childGenerator newTModels)
+        , if List.length newTModels == 0 then 
+            Random.generate GenBackground colorGenerator
+          else
+            Cmd.none
+        ]
     else
       Cmd.none
   )
@@ -74,7 +90,7 @@ type Msg
   = NoOp
   | ChildT (TModel TData)
   | ChangeTargetCount Int
-  | Foo String
+  | GenBackground Color
 
 update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
@@ -86,6 +102,11 @@ update msg model =
     ChangeTargetCount tc ->
       growOrPruneTree tc model
 
+    GenBackground c ->
+      ( { model | bgColor = c }
+      , Cmd.none
+      )
+
     _ -> 
       (model, Cmd.none)
 
@@ -93,29 +114,42 @@ update msg model =
 view : Model -> Html Msg 
 view model = 
   Html.div 
-    [ Html.Attributes.style "background" "white" ]
-    [ viewCanvas model
-    , Html.div []
-        [ Html.input 
-            [ Html.Attributes.type_ "range" 
-            , Html.Attributes.min "0"
-            , Html.Attributes.max "100"
-            , Html.Attributes.step "1"
-            , Html.Attributes.value (String.fromInt model.targetCount)
-            , Html.Events.onInput
-                (String.toInt >> Maybe.map ChangeTargetCount >> Maybe.withDefault NoOp)
-            , Html.Attributes.style "width" "50%"
+    [ Html.Attributes.style "background" "black" 
+    , Html.Attributes.style "height" "100%"
+    , Html.Attributes.style "width" "100%"
+    , Html.Attributes.style "display" "flex"
+    , Html.Attributes.style "justify-content" "center"
+    , Html.Attributes.style "position" "absolute"
+    , Html.Attributes.style "align-items" "center"
+    ]
+    [ Html.div 
+        [] 
+        [ viewCanvas model
+        , Html.div []
+            [ Html.input 
+                [ Html.Attributes.type_ "range" 
+                , Html.Attributes.min "0"
+                , Html.Attributes.max "75"
+                , Html.Attributes.step "1"
+                , Html.Attributes.value (String.fromInt model.targetCount)
+                , Html.Events.onInput
+                    (String.toInt >> Maybe.map ChangeTargetCount >> Maybe.withDefault NoOp)
+                , Html.Attributes.style "width" "100%"
+                , Html.Attributes.style "height" "1px"
+                , Html.Attributes.style "-webkit-appearance" "none"
+                , Html.Attributes.style "background" "white"
+                , Html.Attributes.style "outline" "none"
+                ]
+                []
+            , Html.div [] 
+                [ Html.text (String.fromInt model.targetCount ++ " tiles") ]
             ]
-            []
-        , Html.div [] 
-            [ Html.text (String.fromInt model.targetCount ++ " tiles") ]
         ]
     ]
 
 viewCanvas : Model -> Html Msg 
 viewCanvas model = 
   let
-
     topLeftFrame =
       Frame2d.atPoint (Point2d.pixels -(frameSize/2) (frameSize/2))
         |> Frame2d.reverseY
@@ -123,6 +157,16 @@ viewCanvas model =
     origin =
       Svg.circle2d [Svg.Attributes.fill "black"] 
         (Circle2d.withRadius (pixels 10) Point2d.origin) 
+
+    bgRect = 
+      Svg.rect 
+        [ Svg.Attributes.fill (Color.toCssString model.bgColor) 
+        , Svg.Attributes.width "100%"
+        , Svg.Attributes.height "100%"
+        , Svg.Attributes.x "-50%"
+        , Svg.Attributes.y "-50%"
+        ]
+        []
 
     triSw h = 
       Triangle2d.from h 
@@ -132,23 +176,19 @@ viewCanvas model =
 
     drawTri mt = 
       let 
-        draw = Svg.triangle2d (shapeAttrs mt.data.color)
-        regularTri = draw (mkTriangle mt)
-        mirroredTri m = draw (Triangle2d.mirrorAcross m (mkTriangle mt))
-
+        regularTri = Svg.triangle2d (shapeAttrs mt.data.color) (mkTriangle mt)
         addMirror : Axis2d Pixels () -> Svg Msg -> Svg Msg
         addMirror mAxis orig = Svg.g [] [ orig, Svg.mirrorAcross mAxis orig ]
       in
         Svg.g [] <| [ List.foldr addMirror regularTri mt.mirrors ]
-        -- <| regularTri :: (List.map mirroredTri mt.mirrors)
-
-    twin axis t = 
-      Triangle2d.mirrorAcross axis t
 
     scene =
       Svg.relativeTo topLeftFrame
-        (Svg.g [] 
-           (List.map drawTri model.tModels ++ [])
+        (Svg.g [] <| List.concat 
+          [ [ bgRect ]
+          , List.map drawTri model.tModels
+          , []
+          ]
         )
 
   in
@@ -170,13 +210,17 @@ type alias TModel a =
 
 childGenerator : List (TModel TData) -> Random.Generator (TModel TData)
 childGenerator parents =
-  triRoot |> Random.andThen 
+  triRoot Color.black |> Random.andThen  -- TODO black is dumb, refactor this
     (\newTri -> 
       Random.choose parents
         |> Random.andThen (\(maybeP, _) -> 
           Maybe.map mkChild maybeP |> Maybe.withDefault (Random.constant newTri)
         )
     )
+
+colorGenerator : Random.Generator Color 
+colorGenerator =
+  Random.map3 Color.rgb (Random.float 0 1) (Random.float 0 1) (Random.float 0 1)
 
 mkChild : TModel TData -> Random.Generator (TModel TData)
 mkChild parent =
@@ -193,7 +237,7 @@ mkChild parent =
     plusMinusGenerator = T.chooseWithDefault 1 [1, -1]
 
     dynastyGenerator = 
-      T.chooseWithDefault False [True, False, False]
+      T.chooseWithDefault False [True, False]
 
     makeTheChild childQt mkChildLoc scaleDiff plusMinus dynasty =
       let 
@@ -206,7 +250,7 @@ mkChild parent =
 
         childSat = 0.08 * plusMinus * pr2 childScalevel
 
-        childOpac = 0.4 * plusMinus * pr2 childScalevel
+        childOpac = 0.2 * plusMinus * pr2 childScalevel
           --0.1 * (pr2 -childScalevel - pr2 0)
 
         childColor = 
